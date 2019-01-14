ded07a409260ad7a604d730df79f47dbf6bfad5b
Yuriy <wer3wollf@gmail.com>
2019-01-14 21:42:47 +0200

    model

--- src/App.js
+++ src/App.js
@@ -3,24 +3,58 @@ import logo from './logo.svg';
 import './App.css';
 
 import Grid from 'react-bootstrap/lib/Grid';
-import Row from 'react-bootstrap/lib/Row';
-import Col from 'react-bootstrap/lib/Col';
 import styled from 'styled-components';
+
+import Header from './components/Header'
+import Menu from './components/Menu'
+import Mein from './components/Mein'
+
+const url = process.env.PUBLIC_URL + '/img/bg.jpg'
+
+const HeaderWrapper = styled.header`
+    width: 100%;
+    height: 49%;
+    background-color: #242424;
+    opacity: 0.8;
+`
+
+const MenuWrapper = styled.div`
+    height: 89px;
+    padding-top: 20px;
+`
+
+const MainWrapper = styled.main`
+    height: 600px;
+    padding-top: 130px; 
+    background: url(${url}) no-repeat;
+    background-size: cover;
+`
 
 class App extends Component {
   render() {
     return (
-       <div className="first">
-         <Hello world="World" />
+       <div className="App">
+          <HeaderWrapper>
+            <Grid>
+              <Header />
+            </Grid>
+          </HeaderWrapper>
+          <MenuWrapper>
+            <Grid>
+              <Menu />
+            </Grid>
+          </MenuWrapper>
+          <MainWrapper>
+            <Grid>
+              <Mein />
+            </Grid>
+          </MainWrapper>  
        </div>
+     
     );
   }
 }
 
-function Hello (prop) {
-    return (
-     <h1>Hello {prop.world}</h1>
-  )
-}
+
 
 export default App;
---
+++ src/components/Mein.js
@@ -0,0 +1,66 @@
+import React, { Component } from 'react';
+
+
+import Row from 'react-bootstrap/lib/Row';
+import Col from 'react-bootstrap/lib/Col';
+import styled from 'styled-components';
+
+const Repair = styled.div`
+    color: #ffffff;
+    font-weight: 700;
+    line-height: 50px;
+    font-size: 40px;
+
+    span {
+       display: block;
+       font-size: 24px;
+       line-height: 30px;
+    }
+`
+
+const LightText = styled.div`
+    color: #ffffff;
+    font-size: 18px;
+    font-weight: 300;
+    line-height: 28px;
+    margin-top: 54px;
+`
+
+const CallBtn = styled.button`
+    width: 245px;
+    height: 64px;
+    margin-top: 32px;
+    background-color: #ffa14b;
+    border-radius: 30px;
+    color: #ffffff;
+    font-size: 18px;
+    font-weight: bold;
+
+`
+
+class Main extends React.Component {
+	render(){
+		return (
+           <Row>
+           	 <Col lg={5}>
+           	    <Repair>
+           	    	Качественный ремонт
+                    <span> iphone за 35 минут и гарантия 1 год </span>
+           	    </Repair>
+           	    <LightText>
+           			Оставьте заявку на бесплатную диагностику без очереди,
+                    и получите защитное стекло, стоимостью 1000 рублей,
+                    с установкой в подарок!
+           		</LightText>
+           		<CallBtn>
+           			Отправить заявку!
+           		</CallBtn>
+           	 </Col>
+           	 <Col lg={6} lgOffset={1}>
+           		
+           	 </Col>
+           </Row>
+	    )
+	}
+}
+export default Main 
